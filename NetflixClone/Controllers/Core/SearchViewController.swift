2ecdd9015aa4fa12478cce91c45bfd4c4581b636
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2022-05-06 11:19:06 +0300

    Search functionality added to the project.

--- NetflixClone/Controllers/Core/SearchViewController.swift
+++ NetflixClone/Controllers/Core/SearchViewController.swift
@@ -44,6 +44,8 @@ class SearchViewController: UIViewController {
         
         navigationItem.searchController = searchController
         navigationController?.navigationBar.tintColor = .white
+        
+        searchController.searchResultsUpdater = self
     }
     
 
@@ -89,4 +91,31 @@ extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
+}
+
+
+extension SearchViewController : UISearchResultsUpdating{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
+                  return
+              }
+        APICaller.shared.search(with: query) { results in
+            DispatchQueue.main.async {
+                switch results{
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
 }
--- NetflixClone/Managers/APICaller.swift
+++ NetflixClone/Managers/APICaller.swift
@@ -131,6 +131,28 @@ class APICaller {
         task.resume()
 
     }
+    
+    func search(with query: String,completion: @escaping (Result<[Title],Error>) -> Void){
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        
+        guard let url = URL(string: "\(Constants.BASE_URL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+            } catch  {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        
+        task.resume()
+
+    }
 
 }
 
