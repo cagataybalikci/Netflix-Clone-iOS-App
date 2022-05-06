6b3f65feb0c0a26c25e506e7b3e5b40f980eab99
Çağatay Balıkçı <30704478+cagataybalikci@users.noreply.github.com>
2022-05-06 10:18:57 +0300

    Search bar and search bar controller added to the project.

---
+++ NetflixClone/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,92 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Çağatay Balıkçı on 3.05.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    //MARK: Variables
+    private var titles : [Title] = [Title]()
+    
+    //MARK: TableView
+    private let discoverTable : UITableView = {
+       let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifer)
+       return table
+    }()
+    
+    private let searchController : UISearchController = {
+       let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for Movie or a Tv Show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        fetchDiscoverMovies()
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+    
+    //MARK: Fetch Data to Discover Tableview
+    private func fetchDiscoverMovies(){
+        APICaller.shared.getDiscoverMovies { [weak self] results in
+            switch results{
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+}
+
+
+//MARK: Extensions
+
+extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifer, for: indexPath) as? TitleTableViewCell else {return UITableViewCell()}
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Uknown Title", posterURL: title.poster_path ?? "")
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
---
+++ NetflixClone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixClone
+//
+//  Created by Çağatay Balıkçı on 6.05.2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemCyan
+    }
+    
+
+  
+
+}
